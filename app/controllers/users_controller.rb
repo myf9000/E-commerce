cbfd372bd55afc0213b443ede8920d7ebbb28c99
myf900 <myf9000@vp.pl>
2015-11-27 10:46:34 +0100

    Fixed feed in show user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   	@brr = Array.new
   	order_items.each do |o|
 	  	arr.each do |i|
-	  	  if o.product_id == i
+	  	  if o.product_id == i 
 	  		@brr << o
 	  	  end
   		end
@@ -23,6 +23,7 @@ class UsersController < ApplicationController
     if type == "unfollow"
       current_user.stop_following(@user)
     else
+      current_user.follow(@user)
       current_user.follow(@user)
     end
     redirect_to user_path(current_user)
